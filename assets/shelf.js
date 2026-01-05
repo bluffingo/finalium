924a6bd6f3fc15b633c7c9d4af128564d67778ed
Chaziz <chazpelo2@gmail.com>
2026-01-04 20:12:27 -0500

    g

---
+++ assets/shelf.js
@@ -0,0 +1,5 @@
+// kinda awkward but i don't wanna bloat common.js with shelf-specific code
+
+document.addEventListener("DOMContentLoaded", () => {
+    var feed = document.getElementById("guide-toggle");
+});
