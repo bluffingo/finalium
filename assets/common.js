8df5f28afd78bd94621a0ab5e64eb808f85003a0
Chaziz <chazpelo2@gmail.com>
2026-02-01 17:45:57 -0500

    redo containers, improve guide offset

--- assets/common.js
+++ assets/common.js
@@ -295,12 +295,10 @@ document.addEventListener("DOMContentLoaded", () => {
             var guide = document.getElementById("guide");
             if (guide) {
                 toggleElementDisplay(guide);
-                // if we're on homepage (i think this behavior also applies to profiles?)
-                // this probably isn't the best way to do this. they made use 
-                // of something called content snap widths. this should be looked into later.
-                // chaziz -01/21/2025
-                if (page.classList.contains("home")) {
-                    page.classList.toggle("container-open-guide");
+
+                // if we're on the homepage or on a profile, toggle show-guide in <html>
+                if (page.classList.contains("home") || page.classList.contains("user")) {
+                    document.documentElement.classList.toggle("show-guide");
                 }
             } else {
                 error("where the fuck is the guide???")
