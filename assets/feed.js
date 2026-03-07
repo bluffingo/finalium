c8fa1ad9e7daf0e64ec7dcc77c2bb7d994ed519f
Chaziz <chazpelo2@gmail.com>
2026-03-07 06:53:22 -0500

    accurate slider behavior

--- assets/feed.js
+++ assets/feed.js
@@ -1,6 +1,6 @@
 // shelf slider (hardcoded to feed for the time being)
 
-function normalizeScroll(slider) {
+function normalizeSliderScroll(slider) {
     const first = slider.firstElementChild;
     if (!first) return;
 
@@ -15,6 +15,23 @@ function normalizeScroll(slider) {
     slider.scrollLeft = corrected;
 }
 
+function updateSliderButtons(slider, buttons, scroll) {
+    let leftButton = buttons[0];
+    let rightButton = buttons[1];
+
+    if (scroll < 50) {
+        leftButton.style.display = "none";
+    } else {
+        leftButton.style.display = "flex";
+    }
+
+    if (scroll > (slider.scrollWidth - slider.clientWidth - 100)) {
+        rightButton.style.display = "none";
+    } else {
+        rightButton.style.display = "flex";
+    }
+}
+
 // todo: somehow get this to shut up so we dont waste time checking Every Page
 // for if theres a feed or not.
 document.addEventListener("pageReady", () => {
@@ -27,6 +44,8 @@ document.addEventListener("pageReady", () => {
             const buttons = shelf.querySelectorAll('.shelf-slider-nav');
 
             if (slider) {
+                updateSliderButtons(slider, buttons, slider.scrollLeft);
+
                 let isScrolling = false;
 
                 buttons.forEach(button => {
@@ -44,8 +63,11 @@ document.addEventListener("pageReady", () => {
                             behavior: 'smooth'
                         });
 
+                        properScroll = slider.scrollLeft + (dir * viewport);
+                        updateSliderButtons(slider, buttons, properScroll);
+
                         setTimeout(() => {
-                            normalizeScroll(slider);
+                            normalizeSliderScroll(slider);
                             isScrolling = false;
                             buttons.forEach(b => b.disabled = false);
                         }, 350);
